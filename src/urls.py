e71c6bf469bf7838220b87852a8836a75969a063
Jaakko Tulkki <jaakko.tulkki@gmail.com>
2016-02-27 01:06:54 +0100

    added urls for books

--- src/urls.py
+++ src/urls.py
@@ -4,6 +4,7 @@ from anillo.handlers.routing import optionized_url as url, context
 from controllers.index import Index
 from controllers.user import Login, Register, List
 from controllers.author import ListCreateAuthors, DetailUpdateDeleteAuthor
+from controllers.book import CreateListBook, DetailUpdateDeleteBook
 
 urls = [
     context("/api/v1", [
@@ -13,6 +14,8 @@ urls = [
         url("/users", List(), methods=["get"]),
         url("/authors", ListCreateAuthors(), methods=["get", "post"]),
         url("/authors/<int:pk>", DetailUpdateDeleteAuthor(), methods=["get", "delete", "put"]),
+        url("/books", CreateListBook(), methods=["get", "post"]),
+        url("/books/<int:pk>", DetailUpdateDeleteBook(), methods=["get", "delete", "put"]),
 
     ]),
 ]
